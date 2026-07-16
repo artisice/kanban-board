6a507056897847a37edbe640dd6f0042df6962c9
Arthur <arthur-lebedev@mail.ru>
2026-07-16 06:05:34 +0300

    fix: add .htaccess for Apache routing and disable sockets in production

--- client/src/api/socket.js
+++ client/src/api/socket.js
@@ -1,3 +1,9 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('http://localhost:3001');
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
+
+export const socket = SOCKET_URL ? io(SOCKET_URL) : {
+    emit: () => {},
+    on: () => {},
+    off: () => {}
+};
